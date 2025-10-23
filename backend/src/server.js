20b65b8b09f282d3def60526706fa5ff22448ade
MihanN1 <krutoysansn1@gmail.com>
2025-10-23 18:08:01 +0300

    prepared deployment & convenience scripts for me to use

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js"
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (_,res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+    });
+};
 
 app.use("/api/messages", messageRoutes);
 app.use("/api/auth", authRoutes);

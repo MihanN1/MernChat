1cfe0d3a13f209791dedc27cece22dfa88674f19
MihanN1 <krutoysansn1@gmail.com>
2025-10-26 17:01:01 +0300

    refactored so it can check the env variables' existance

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("mongodb connected successfully: ", conn.connection.host);
+    const { MONGO_URI } = ENV;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("MONGODB CONNECTED:", conn.connection.host);
     } catch (error) {
         console.error("error connected to mongodb: ", error);
         process.exit(1);

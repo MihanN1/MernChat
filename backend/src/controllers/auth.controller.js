7f5d25d577287e1c1eb63c0cde27e1dcde9d3f9b
MihanN1 <krutoysansn1@gmail.com>
2025-12-05 11:45:04 +0300

    Fixed my stupid coding mistakes. In a few days will refactor code, make a README(in which there will be a lot of info about the future of this project)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
-import { generateToken } from "../lib/utils";
-import User from "../models/User";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import {sendWelcomeEmail} from "../emails/emailHandlers.js";
 import {ENV} from "../lib/env.js";
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller";
-import { protectRoute } from "../middleware/auth.middleware";
-import { arcjetProtection } from "../middleware/arcjet.middleware";
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 

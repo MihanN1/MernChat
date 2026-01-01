af124ab56202757973647e30571358b5f5cb1c2c
MihanN1 <krutoysansn1@gmail.com>
2026-01-01 21:11:32 +0300

    Secure dependencies hotfix 1.0.10.3 FROM NOW HOTFIXES WILL BE NUMBERED AS A FORTH NUMBER FOR A VERSION.

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -5,6 +5,9 @@ export const resendClient = new Resend(ENV.RESEND_API_KEY);
 
 let sender = {};
 if (ENV.NODE_ENV === 'development') {
+    if (!ENV.EMAIL_FROM) {  
+        throw new Error('EMAIL_FROM is required in development environment');  
+    } 
     sender = {
         email: ENV.EMAIL_FROM,
         name: ENV.EMAIL_FROM_NAME
